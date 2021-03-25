d4b2adeac54931645ed91997985154619de8d297
Vernie Namca <vernie@mybusybee.net>
2021-03-25 21:28:55 +0800

    Fixed problem populating of benefit status dropdown
    Fixed problem populating of benefit status dropdown.

--- PVAOWeb/Scripts/overremittance.js
+++ PVAOWeb/Scripts/overremittance.js
@@ -14,9 +14,22 @@
     },
     form.getBenefitStatuses = function() {
         $.get("http://localhost:55653/api/overremittance/getbenefitstatuses", function (data) {
-            for (var i = 0; i < data.length; i++) {
-                $('#benefitStatuses').append("<option>" + data[i].description + " " + data[i].prefix + " " + data[i].claimant + "</option>");
-            }
+            var benefitStatus = data.filter(function (item) {
+                return item.prefix == "DW" || item.prefix == "DC" || item.prefix == "TP";
+            });
+
+            var htmlContent = '<a id="bs-dropdown-action" class="btn btn-secondary dropdown-toggle" href="" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">-- Benefit Status --</a>';
+                htmlContent += '<div class="dropdown-menu bs-dropdown-mnu" aria-labelledby="dropdownMenuLink">';
+                    htmlContent += `<a id="bs-all" data-id="0" data-description="-- Benefit Status --" class="dropdown-item" href="" onclick="return dropdownAction(this, 'bs-dropdown-action');">-- Benefit Status --</a>`;
+
+                    for (var x = 0; x < benefitStatus.length; x++) {
+                        var description = `${benefitStatus[x].prefix} - ${benefitStatus[x].description} ( ${benefitStatus[x].claimant} )`;
+
+                        htmlContent += `<a id="bs-${benefitStatus[x].id}" data-id="${benefitStatus[x].id}" data-description="${description}" class="dropdown-item" href="" onclick="return dropdownAction(this, 'bs-dropdown-action');">${description}</a>`;
+                    }
+                htmlContent += '</div>';
+
+            $('#over-remittance-list .bs-dropdown').html(htmlContent);
         });
     }
 }
