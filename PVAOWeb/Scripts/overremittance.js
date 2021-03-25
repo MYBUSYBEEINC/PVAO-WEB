8ea29258cb0e0a42eb76ee2dbdfcef82f6eccd25
Cave, Don (TaguigCity1634) <don.cave@towerswatson.com>
2021-03-25 20:09:58 +0800

    added overremittance.js codes; updated Overremittance list

--- PVAOWeb/Scripts/overremittance.js
+++ PVAOWeb/Scripts/overremittance.js
@@ -2,9 +2,16 @@
     var form = this;
 
     form._construct = function () {
-
+        form.getBenefitStatuses();
     },
     form._events = function () {
+    },
+    form.getBenefitStatuses = function() {
+        $.get("http://localhost:55653/api/overremittance/getbenefitstatuses", function (data) {
+            for (var i = 0; i < data.length; i++) {
+                $('#benefitStatuses').append("<option>" + data[i].description + " " + data[i].prefix + " " + data[i].claimant + "</option>");
+            }
+        });
     }
 }
 
