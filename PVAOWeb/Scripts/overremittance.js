d30ba35c6ffa96c87eda39271e0796f1c69af9c6
Cave, Don (TaguigCity1634) <don.cave@towerswatson.com>
2021-03-27 12:30:19 +0800

    globalize webApiUrl; separated loading of control from loading the values;

--- PVAOWeb/Scripts/overremittance.js
+++ PVAOWeb/Scripts/overremittance.js
@@ -1,4 +1,17 @@
-﻿var OverRemittance = function () {
+﻿//var Constants = function () {
+//    var form = this;
+//    form._construct = function () {
+        
+//    };
+//    form.getWebApiUrl = function() {
+//        return '@System.Configuration.ConfigurationManager.AppSettings["webApiBaseUrl"]';
+//    };
+//}
+//var constants = new Constants();
+//constants._construct();
+//alert(constants.getWebApiUrl);
+
+var OverRemittance = function () {
     var form = this;
 
     form._construct = function () {
@@ -9,10 +22,10 @@
         //    e.preventDefault();
         //    form.getBenefitStatuses();
         //});
-        $(window).on("load", form.getBenefitStatuses);
+        $(window).on("load", form.loadBenefitStatusesControl); //loadBenefitStatusesControl getBenefitStatuses
         //form.getBenefitStatuses();
     },
-    form.getBenefitStatuses = function() {
+    form.loadBenefitStatusesControl = function() {
         $.get("http://localhost:55653/api/overremittance/getbenefitstatuses", function (data) {
             var benefitStatus = data.filter(function (item) {
                 return item.prefix == "DW" || item.prefix == "DC" || item.prefix == "TP";
@@ -22,17 +35,44 @@
                 htmlContent += '<div class="dropdown-menu bs-dropdown-mnu" aria-labelledby="dropdownMenuLink">';
                     htmlContent += '<a id="bs-all" data-id="0" data-description="-- Benefit Status --" class="dropdown-item" href="" onclick="return dropdownAction(this, bs-dropdown-action);">-- Benefit Status --</a>';
 
-                    for (var x = 0; x < benefitStatus.length; x++) {
-                        var description = `${benefitStatus[x].prefix} - ${benefitStatus[x].description} ( ${benefitStatus[x].claimant} )`;
+                    //for (var x = 0; x < benefitStatus.length; x++) {
+                    //    var description = `${benefitStatus[x].prefix} - ${benefitStatus[x].description} ( ${benefitStatus[x].claimant} )`;
 
-                        htmlContent += `<a id="bs-${benefitStatus[x].id}" data-id="${benefitStatus[x].id}" data-description="${description}" class="dropdown-item" href="" onclick="return dropdownAction(this, 'bs-dropdown-action');">${description}</a>`;
-                    }
+                    //    htmlContent += `<a id="bs-${benefitStatus[x].id}" data-id="${benefitStatus[x].id}" data-description="${description}" class="dropdown-item" href="" onclick="return dropdownAction(this, 'bs-dropdown-action');">${description}</a>`;
+                    //}
                 htmlContent += '</div>';
+
+            $('#over-remittance-list .bs-dropdown').html(htmlContent);
+        });
+    },
+    form.getBenefitStatuses = function () {
+        $.get(window.webApiUrl + "overremittance/getbenefitstatuses", function (data) {
+            var benefitStatus = data.filter(function (item) {
+                return item.prefix == "DW" || item.prefix == "DC" || item.prefix == "TP";
+            });
+            alert(window.webApiUrl);
+            //var htmlContent = '<a id="bs-dropdown-action" class="btn btn-secondary dropdown-toggle" href="" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">-- Benefit Status --</a>';
+            //htmlContent += '<div class="dropdown-menu bs-dropdown-mnu" aria-labelledby="dropdownMenuLink">';
+            //htmlContent += '<a id="bs-all" data-id="0" data-description="-- Benefit Status --" class="dropdown-item" href="" onclick="return dropdownAction(this, bs-dropdown-action);">-- Benefit Status --</a>';
+
+            var dropdownElement = document.getElementsByClassName()('dropdown-menu bs-dropdown-mnu');
+            var htmlContent = dropdownElement;
+
+            for (var x = 0; x < benefitStatus.length; x++) {
+                var description = `${benefitStatus[x].prefix} - ${benefitStatus[x].description} ( ${benefitStatus[x].claimant} )`;
+
+                htmlContent += `<a id="bs-${benefitStatus[x].id}" data-id="${benefitStatus[x].id}" data-description="${description}" class="dropdown-item" href="" onclick="return dropdownAction(this, 'bs-dropdown-action');">${description}</a>`;
+            }
+            //htmlContent += '</div>';
 
             $('#over-remittance-list .bs-dropdown').html(htmlContent);
         });
     }
 }
 
-var myClass = new OverRemittance();
-myClass._construct();
+var overremittance = new OverRemittance();
+overremittance._construct();
+
+$(document).ready(function() {
+    overremittance.getBenefitStatuses();
+});
