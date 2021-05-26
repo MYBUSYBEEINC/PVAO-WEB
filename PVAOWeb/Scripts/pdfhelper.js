66ebd3376a3d198ea2d4c99255b2523dbb7f6a9b
Cave, Don (TaguigCity1634) <don.cave@towerswatson.com>
2021-05-26 18:49:51 +0800

    customized header for Pdf

--- PVAOWeb/Scripts/pdfhelper.js
+++ PVAOWeb/Scripts/pdfhelper.js
@@ -2,7 +2,15 @@
     var form = this;
     form.exportTableToPdf = function (table, pdfName, orientation) {
         var doc = new jsPDF(orientation);
-        doc.autoTable({ html: table });
+        doc.autoTable({
+            html: table,
+            didParseCell: function (table) {
+                if (table.section === "head") {
+                    table.cell.styles.fillColor = [118, 160, 60];
+                    table.cell.styles.textColor = [0, 0, 0];
+                }
+            }
+        });
         doc.save(pdfName);
     }
 }
